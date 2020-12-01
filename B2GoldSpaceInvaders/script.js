26532a711444d8fa9114f525dc557099da8f4586
Lucas van Diepen <lucascool@ziggo.nl>
2020-12-01 23:56:06 +0100

    Updated script.js

--- B2GoldSpaceInvaders/script.js
+++ B2GoldSpaceInvaders/script.js
@@ -1,7 +1,72 @@
+//Player values
 let playerImage;
-let playerDirection = 50;
+let player;
+let playerStartHeight = 820;
 
-//document.onkeydown = keys;
+//Bullet values
+let bullets = [];
+
+let screenWidth = 1200;
+let screenHeight = 900;
+
+class Bullet{
+    speed = 10;
+    x = -1;
+    y = playerStartHeight;
+
+    constructor(_x)
+    {
+        x = _x;
+    }
+
+    Update()
+    {
+
+    }
+
+    Draw()
+    {
+
+    }
+}
+
+class Player {
+    speed = 5;
+    x = 0;
+    sprite = null;
+    moveDirection = 0;
+
+    constructor(_playerImage = playerImage)
+    {
+        this.sprite = _playerImage;
+        this.x = screenWidth / 2;
+    }
+
+    Update()
+    {
+        if((this.x <= 100 && this.moveDirection != 1) || (this.x >= screenWidth - 100 && this.moveDirection != -1))
+        {
+            //freeze movement
+        }
+        else
+        {
+            this.x += this.moveDirection * this.speed;
+        }
+        
+        
+    }
+
+    Draw()
+    {
+        playerImage.resize(50, 50);
+        image(player.sprite, player.x, playerStartHeight);
+    }
+
+    Shoot()
+    {
+        bullets.push(Bullet(this.x));
+    }
+}
 
 function preload()
 {
@@ -9,17 +74,47 @@ function preload()
 }
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(screenWidth, screenHeight);
+    player = new Player();
 }
-  
+
+function update(){
+    //Update all game objects
+
+    //Update player
+    player.Update();
+
+    //Update bullets
+    for(var i = bullets.length - 1; i>=0; i--)
+    {
+        bullets[i].Update();
+    }
+}
+
 function draw() {
+    //Update game logic
+    update()
+
+    //Draw
     background(220);
-    playerImage.resize(50, 50);
-    image(playerImage, playerDirection, 100);
+    
+    //draw player
+    player.Draw();
+
+    for(var i = 0; i < bullets.length; i++)
+    {
+        bullets[i].Draw();
+    }
 }
 
 function keyPressed()
 {
-    if(keyCode == LEFT_ARROW) playerDirection = -1;
-    else if(keyCode == RIGHT_ARROW) playerDirection = 1;
+    if(keyCode == LEFT_ARROW) player.moveDirection = -1;
+    else if(keyCode == RIGHT_ARROW) player.moveDirection = 1;
+}
+
+function keyReleased()
+{
+    if(keyCode == LEFT_ARROW) player.moveDirection = 0;
+    if(keyCode == RIGHT_ARROW) player.moveDirection = 0;
 }
