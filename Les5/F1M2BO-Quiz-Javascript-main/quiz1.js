606e7eb3009d0bebec2cb5cb3bfdd9bbcbafd618
Lucas van Diepen <lucascool@ziggo.nl>
2020-12-17 11:16:35 +0100

    Added Les5

---
+++ Les5/F1M2BO-Quiz-Javascript-main/quiz1.js
@@ -0,0 +1,81 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://daf9627eib4jq.cloudfront.net/app/uploads/2018/07/LAM_Exterieur-2-Sjaak-Henselman-1000x667.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Voor welke leeftijd is het LAM?",
+				"answers": [{
+						"answer": "40 jaar t/m 59 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "20 jaar t/m 39 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "Voor iedereen",
+						"feedback": true
+					},
+					{
+						"answer": "0 jaar t/m 19",
+						"feedback": false
+					},
+					{
+						"answer": "60 jaar of ouder",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoelang duurt een bezoek aan het LAM meestal?",
+				"answers": [{
+						"answer": "45 minuten",
+						"feedback": true
+					},
+					{
+						"answer": "35 minuten",
+						"feedback": false
+					},
+					{
+						"answer": "60 minuten",
+						"feedback": false
+					},
+					{
+						"answer": "55 minuten",
+						"feedback": false
+					},
+					{
+						"answer": "90 minuten",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoe duur is een ticket",
+				"answers": [{
+						"answer": "Gratis",
+						"feedback": true
+					},
+					{
+						"answer": "€10",
+						"feedback": false
+					},
+					{
+						"answer": "€2,50",
+						"feedback": false
+					},
+					{
+						"answer": "€5 of €7,50",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
